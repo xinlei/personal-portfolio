1565de1ca9f3c19ef423009b8d5f4daa48ef2f5f
xinlei <wang.xinlei15@gmail.com>
2015-01-25 23:57:27 -0800

    changed background

--- js/default.js
+++ js/default.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	var viewportHeight = $(window).height();
 
 	$(".left-lines-container").css("height", documentHeight);
+	$(".right-top-lines-container").css("height", documentHeight);
 	$(".right-lines-container").css("height", documentHeight-viewportHeight/2);
 
 	$(window).on('resize', function(){
@@ -12,6 +13,55 @@ $(document).ready(function() {
 		var viewportHeight = win.height();
 
 		$(".left-lines-container").css("height", documentHeight);
+		$(".right-top-lines-container").css("height", documentHeight);
 		$(".right-lines-container").css("height", documentHeight-viewportHeight/2); 
-	}); 
+	});
+
+	$(".detail").hover(
+		function(){
+			$(".right-top-line").css("stroke-width", 3);
+			$(".right-top-line").css("stroke-opacity", 1);
+			$(".left-line").css("stroke-width", 3);
+			$(".left-line").css("stroke-opacity", 1);
+		},
+		function(){
+			$(".right-top-line").css("stroke-width", 1);
+			$(".right-top-line").css("stroke-opacity", .2);
+			$(".left-line").css("stroke-width", 1);
+			$(".left-line").css("stroke-opacity", .2);
+		}
+	);
+	$(".code").hover(
+		function(){
+			$(".right-line").css("stroke-width", 3);
+			$(".right-line").css("stroke-opacity", 1);
+			$(".left-line").css("stroke-width", 3);
+			$(".left-line").css("stroke-opacity", 1);
+		},
+		function(){
+			$(".right-line").css("stroke-width", 1);
+			$(".right-line").css("stroke-opacity", .2);
+			$(".left-line").css("stroke-width", 1);
+			$(".left-line").css("stroke-opacity", .2);
+		}
+	);  
+	
+	$(".code-1").hover(
+		function(){
+			$(".right-line").css("stroke-width", 3);
+			$(".right-line").css("stroke-opacity", 1);
+			$(".right-top-line").css("stroke-width", 3);
+			$(".right-top-line").css("stroke-opacity", 1);
+			$(".left-line").css("stroke-width", 3);
+			$(".left-line").css("stroke-opacity", 1);
+		},
+		function(){
+			$(".right-line").css("stroke-width", 1);
+			$(".right-line").css("stroke-opacity", .2);
+			$(".right-top-line").css("stroke-width", 1);
+			$(".right-top-line").css("stroke-opacity", .2);
+			$(".left-line").css("stroke-width", 1);
+			$(".left-line").css("stroke-opacity", .2);
+		}
+	);  
 });
